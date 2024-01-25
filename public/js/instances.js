ad74914be409e58bc5947a7792a9d2458afb91a3
Eric_PC_FIXE <edelhaye93@gmail.com>
2024-01-25 20:23:08 +0100

    attack selection works but logs need to be changed yet

---
+++ public/js/instances.js
@@ -0,0 +1,4 @@
+export let selectIceBeam = document.querySelector(".iceBeam");
+export let selectSurf = document.querySelector(".surf")
+export let selectRest = document.querySelector(".rest")
+export let selectConfuseRay = document.querySelector(".confuseRay")
--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,39 @@
 import { icebeam, synthesis, confuseRay, surf, rest, sludgeBomb, petalDance, sleepPowder, healthCheck } from "./functions.js";
 import { Venusaur } from "./pokemon.js";
 import { Lapras } from "./pokemon.js";
+import { selectConfuseRay, selectSurf, selectRest, selectIceBeam } from "./instances.js";
 
-let battleText = document.querySelector(".textContainer");
-let initialText = document.createElement("p");
-initialText.innerText = "A wants to Battle B!";
-initialText.style.color = "white"
-battleText.appendChild(initialText);
+let battleText = document.querySelector(".battleText");
+// let initialText = document.createElement("p");
+// initialText.innerText = "A wants to Battle B!";
+// initialText.style.color = "white"
+// battleText.appendChild(initialText);
+// function appendLog(message) {
+//    let p = document.createElement("p");
+//    p.innerText = message;
+//    battleText.appendChild(p);
+// }
+
+
+function appendLog(message) {
+   let p = document.createElement("p");
+   p.innerText = message;
+   battleText.appendChild(p);
+}
+
+appendLog("A wants to battle B!");
+appendLog("start!");
+appendLog("Venusaur wants to battle !!");
+appendLog("Venusaur's health : " + Venusaur.max_health);
+appendLog("Lapras's health : " + Lapras.max_health);
+
+
+console.log = (function(originalLogFunc) {
+   return function(message) {
+       originalLogFunc(message); // Logs the message to the console
+       appendLog(message); // Appends the message to the battleText div
+   };
+}(console.log));
 
 
 
@@ -18,58 +45,182 @@ console.log("Venusaur wants to battle !!");
 console.log("Venusaur's health : " + Venusaur.max_health);
 console.log("Lapras's health : " + Lapras.max_health);
  
- while (Venusaur.current_health > 0 && Lapras.current_health > 0){
-    
-    let action = prompt("What should Lapras do?: \n1.Ice Beam  \n2.Surf ")
- 
-    switch(action) {
-       case"1":
-           icebeam();
-           break;
-       case"2":
-           surf()
-           break;
-           default:   
-          console.log("Invalid choice. Please select an action with 1 or 2")
-             
-           }
+
+// Lapras IceBeam attack //           
+  selectIceBeam.addEventListener("click", function() {
+      icebeam();
+      appendLog("Ice Beam was used.");
            
+      healthCheck();
+
+      if (Venusaur.current_health < 1){
+        console.log("Venusaur fainted....Lapras wins!");
+        
+  
+     }else{
+  
+     }
  
+ 
+ let VenusaurRandom = Math.floor(Math.random()*4);
+ console.log(VenusaurRandom);
+  if (VenusaurRandom == 0){
+     petalDance();
+  }else if (VenusaurRandom == 1){
+     synthesis();
+  }else if (VenusaurRandom == 2){
+     sleepPowder();
+  }else if (VenusaurRandom == 3) {
+     sludgeBomb();
+  }
+  
+  healthCheck();
+  
+  if (Lapras.current_health <= 0) {
+     console.log("Lapras fainted....Venusaur wins! ");
+     
+  
+  
+    } 
+  });
+  
+
+
+
+
+// Lapras Surf attack //    
+  selectSurf.addEventListener("click", function() {
+      surf();
+      appendLog("Surf was used.");
+      
+      healthCheck();
+
+      if (Venusaur.current_health < 1){
+        console.log("Venusaur fainted....Lapras wins!");
+        
+  
+     }else{
+  
+     }
+ 
+ 
+ let VenusaurRandom = Math.floor(Math.random()*4);
+ console.log(VenusaurRandom);
+  if (VenusaurRandom == 0){
+     petalDance();
+  }else if (VenusaurRandom == 1){
+     synthesis();
+  }else if (VenusaurRandom == 2){
+     sleepPowder();
+  }else if (VenusaurRandom == 3) {
+     sludgeBomb();
+  }
+  
+  healthCheck();
+  
+  if (Lapras.current_health <= 0) {
+     console.log("Lapras fainted....Venusaur wins! ");
+     
+  
+  
+    }
+
+  });
+  
+
+
+
+
+// Lapras Rest attack //    
+  selectRest.addEventListener("click", function() {
+      rest();
+      appendLog("Rest was used.");
+
+      healthCheck();
+
+      if (Venusaur.current_health < 1){
+        console.log("Venusaur fainted....Lapras wins!");
+        
+  
+     }else{
+  
+     }
+ 
+ 
+ let VenusaurRandom = Math.floor(Math.random()*4);
+ console.log(VenusaurRandom);
+  if (VenusaurRandom == 0){
+     petalDance();
+  }else if (VenusaurRandom == 1){
+     synthesis();
+  }else if (VenusaurRandom == 2){
+     sleepPowder();
+  }else if (VenusaurRandom == 3) {
+     sludgeBomb();
+  }
+  
+  healthCheck();
+  
+  if (Lapras.current_health <= 0) {
+     console.log("Lapras fainted....Venusaur wins! ");
+     
+  
+  
+    }
+
+  });
+
+
+
+
+
+
+// Lapras confusion attack //    
+  selectConfuseRay.addEventListener("click", function() {
+   confuseRay();
+   appendLog("Rest was used.");
+
+   healthCheck();
+
+      if (Venusaur.current_health < 1){
+        console.log("Venusaur fainted....Lapras wins!");
+        
+  
+     }else{
+  
+     }
+ 
+ 
+ let VenusaurRandom = Math.floor(Math.random()*4);
+ console.log(VenusaurRandom);
+  if (VenusaurRandom == 0){
+     petalDance();
+  }else if (VenusaurRandom == 1){
+     synthesis();
+  }else if (VenusaurRandom == 2){
+     sleepPowder();
+  }else if (VenusaurRandom == 3) {
+     sludgeBomb();
+  }
+  
+  healthCheck();
+  
+  if (Lapras.current_health <= 0) {
+     console.log("Lapras fainted....Venusaur wins! ");
+     
+  
+  
+    }
+   
+
+});
+
+  
        
     
     
      
- healthCheck();
 
-     if (Venusaur.current_health < 1){
-       console.log("Venusaur fainted....Lapras wins!");
-       break;
- 
-    }else{
- 
-    }
-
-
-let VenusaurRandom = Math.floor(Math.random()*4);
-console.log(VenusaurRandom);
- if (VenusaurRandom == 0){
-    petalDance();
- }else if (VenusaurRandom == 1){
-    synthesis();
- }else if (VenusaurRandom == 2){
-    sleepPowder();
- }else if (VenusaurRandom == 3) {
-    sludgeBomb();
- }
- 
- healthCheck();
- 
- if (Lapras.current_health <= 0) {
-    console.log("Lapras fainted....Venusaur wins! ");
-    break;
- 
- 
-   } 
-};    
+// };    
  
 
